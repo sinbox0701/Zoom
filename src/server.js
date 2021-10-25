38234e1a66dfb1ee7e2f9e2161a3bebb08bdbdcd
sinbox0701 <psh090953@gmail.com>
2021-10-25 23:19:21 +0900

    Front SetUp

--- src/server.js
+++ src/server.js
@@ -2,6 +2,18 @@ import express from "express";
 
 const app = express();
 
-console.log("Hello");
+app.set("view engine", "pug");
+//pug를 front 엔진으로 설정
+app.set("views",__dirname + "/views");
+//template 위치 알려줌
 
-app.listen(3000);
+app.use("/public",express.static(__dirname + "/public"));
+//파일 공유
+app.get("/", (req,res) => res.render("home"));
+//공유한거 home.pug로 렌더링
+app.get("/*", (req, res) => res.redirect("/"));
+//home만 사용하기위해 설정
+
+const handleListen = () => console.log(`Listening on http://loaclhost:3000`);
+
+app.listen(3000,handleListen);
