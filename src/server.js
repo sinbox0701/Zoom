533bf6512f2dfb136bf0f3ee1c443308935693c6
sinbox0701 <psh090953@gmail.com>
2021-10-26 16:46:38 +0900

    wss Setting

--- src/server.js
+++ src/server.js
@@ -1,19 +1,19 @@
 import express from "express";
+import http from "http";
+import WebSocket, { WebSocketServer } from "ws";
 
 const app = express();
 
 app.set("view engine", "pug");
-//pug를 front 엔진으로 설정
 app.set("views",__dirname + "/views");
-//template 위치 알려줌
 
 app.use("/public",express.static(__dirname + "/public"));
-//파일 공유
+
 app.get("/", (req,res) => res.render("home"));
-//공유한거 home.pug로 렌더링
 app.get("/*", (req, res) => res.redirect("/"));
-//home만 사용하기위해 설정
 
-const handleListen = () => console.log(`Listening on http://loaclhost:3000`);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+const server = http.createServer(app);//http server
+const wss = new WebSocketServer({server});//websocket server
 
-app.listen(3000,handleListen);
+server.listen(3000,handleListen);
