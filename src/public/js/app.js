30a68a995ef8d7e997b8d794cfc49ed14bcf1b8b
sinbox0701 <psh090953@gmail.com>
2021-10-27 21:00:11 +0900

    WebSocket Event

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,2 @@
-alert("ho");
+const socket = new WebSocket(`ws://${window.location.host}`); 
+//front에서 back을 연결하는 방법
--- src/server.js
+++ src/server.js
@@ -16,4 +16,11 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);//http server
 const wss = new WebSocketServer({server});//websocket server
 
+function handleConnection(socket){
+    console.log(socket);
+    //socket과 실시간 소통
+}
+wss.on("connection",handleConnection);
+//connection event 추가
+//connection이 되면 hanldleConnection실행
 server.listen(3000,handleListen);
