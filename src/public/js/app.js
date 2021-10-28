23539695c1a6166065fe1d5deff8fe20a7e5471c
sinbox0701 <psh090953@gmail.com>
2021-10-28 11:17:24 +0900

    Chat Completed

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,8 +1,13 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+
 const socket = new WebSocket(`ws://${window.location.host}`); 
 
-socket.addEventListener("open",()=>{
-    console.log("Connected to Server ✅");
-});
+function handleOpen(){
+    console.log("Connected to Server ✅");    
+}
+
+socket.addEventListener("open",handleOpen);
 socket.addEventListener("message",(message)=>{
     console.log("New Message: ",message.data);
 });
@@ -10,7 +15,11 @@ socket.addEventListener("close",()=>{
     console.log("Disconnected from Server ❌");
 });
 
-setTimeout(()=>{
-    socket.send("hello from the browser!");
-    //back end(server.js)의 message
-},10000);
+function handleSubmit(event){
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(input.value);
+    input.value="";
+}
+
+messageForm.addEventListener("submit",handleSubmit);
--- src/server.js
+++ src/server.js
@@ -16,15 +16,23 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);//http server
 const wss = new WebSocketServer({server});//websocket server
 
-wss.on("connection",(socket)=>{
-    console.log("Connected to Browser ✅");
-    socket.on("close",()=>console.log("Disconnected from Server ❌"));
-    socket.on("message", (message) => {
-        console.log(message);
-    });
 
-    socket.send("hello!!!");
-    //front end(app.js)의 message.data
+function onSocketClose() {
+    console.log("Disconnected from the Browser ❌");
+}
+  
+const sockets = [];
+//여러 브라우저의 socket을 전달 받음
+
+wss.on("connection",(socket)=>{
+    sockets.push(socket);
+    //sockets에 socket을 전달
+    console.log("Connected to Browser ✅");
+    socket.on("close",onSocketClose);
+    socket.on("message", (message)=>{
+        sockets.forEach((aSocket) => aSocket.send(message.toString()));
+    });
+    //각 브라우저의 메세지를 서로에게 보내줄수 있도록 작동
 });
 
 server.listen(3000,handleListen);
