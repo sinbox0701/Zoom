71ab11b1a24dc1e918f93612a451b42670e7214e
sinbox0701 <psh090953@gmail.com>
2021-11-05 15:14:29 +0900

    User  Count

--- src/server.js
+++ src/server.js
@@ -14,8 +14,6 @@ app.get("/*", (req, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
-//Adapter --> 같은 화면을 보고있는 것 같지만 각각 다른 서버 ==> Adapter를 이용해 서버 공유
-// rooms와 socketids를 볼수 있음
 
 const publicRooms = () => {
   const {
@@ -32,6 +30,11 @@ const publicRooms = () => {
   return publicRooms;
 }
 
+const countRoom = (roomName) => {
+  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+// wsServer에서 돌아가고 있는 모든 소켓 수 return
+
 wsServer.on("connection",(socket) => {
   socket["nickname"] = "Anon";
   socket.onAny((event) => {
@@ -42,11 +45,11 @@ wsServer.on("connection",(socket) => {
     socket["nickname"] = nickname;
     socket.join(roomName);
     done();
-    socket.to(roomName).emit("welcome",socket.nickname);
+    socket.to(roomName).emit("welcome",socket.nickname,countRoom(roomName));
     wsServer.sockets.emit("room_change", publicRooms());
     //to every socket connencted
     socket.on("disconnecting", () => {
-      socket.rooms.forEach((room) => socket.to(room).emit("bye",socket.nickname));
+      socket.rooms.forEach((room) => socket.to(room).emit("bye",socket.nickname,countRoom(roomName)-1));
     });
     socket.on("disconnect", () => {
       wsServer.sockets.emit("room_change", publicRooms());
